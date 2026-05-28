7c9bbafd8d463ed2b0411b63a811bf4c0306616d
Starlyyy <izabella.costa.tds2023@gmail.com>
2026-05-28 14:21:26 -0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../app/core/Autoload.php';
+require_once __DIR__ . '/../app/config/Config.php';
+
+use app\core\Router;
+
+$router = new Router();
+
+$router->get('/homepage', '');
