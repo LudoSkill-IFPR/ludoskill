d4a52578fec312205ca117c660047caa93e27e0c
Re <134728551+Rebs-Sch@users.noreply.github.com>
2026-08-23 01:13:57 -0300

    Reformulação e restilização da tela de login
    Rebeca refez a interface e a estrutura do html, para melhorar a sintaxe e a aparência.

--- app/views/entrada/login.php
+++ app/views/entrada/login.php
@@ -3,50 +3,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/homepage.css">
+    <link rel="stylesheet" href="assets/css/geralEntrada.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+
     <title>LudoSkill</title>
 </head>
 <body>
     <header>
-        <div id="logo">
-            <div id="logozinha"><img src="assets/imagens/skillo.png" alt=""></div>
-            <a href="public"><h2>LudoSkill</h2></a>
-        </div>
+        <nav>
+            <!--volta para a pagina inicial-->
+            <a id="homepage" href="homepage.php">
+                <img id="icone" src="assets/imagens/icone-ludoskill-transparente.png" alt="icone do ludoskill">
+                <img id="logo" src="assets/imagens/logo-ludoskill.png" alt="logo do ludoskill">
+            </a>
 
-        <div>
-            <label for="temas" class="botao">Temas</label>
-            <select name="temas" id="temas" class="botao">
-                <option value="padrao">Padrão</option>
-                <option value="escuro">Escuro</option>
-                <option value="claro">Claro</option>
-            </select>
-        </div>
-        
+            <!--decide o tema do site-->
+            <div id="temas">
+                <label for="temas">Temas</label>
+                <select name="temas" id="temas" class="seletor">
+                    <option hidden disabled value="padrao">Padrão</option>
+                    <option value="escuro">Escuro</option>
+                    <option value="claro">Claro</option>
+                </select>
+            </div>
+        </nav>
     </header>
+
     <main>
-        <form action="<?= URL_BASE ?>/logar" method="post">
-            <img src="assets/imagens/skillo.png" alt=""><br>
-            <div id="erro">Erro</div>
-            <label for="email">Email:</label><br>
-            <input type="email" name="email" id="email" placeholder="skillo@bolotas.com"><br>
-
-            <label for="senha">Senha:</label><br>
-            <input type="password" name="senha" id="senha" placeholder="euAMObolotas123!">
-
-            <div id="checkbox">
-                <input type="checkbox" name="lembrar" id="lembrar">
-                Lembrar de mim
-            </div>
+        <div class="card">
+            <form action="" method="post">
+                <img id="skillo" src="assets/imagens/icone-ludoskill-transparente.png" alt="">
+                <h1>Bem vindo de volta!</h1>
             
-
-            <div class="espacinho"></div>
-
-            <button id="enviar">Entrar</button><br>
-            <div id="centraliza">
-                <div><a href="">Esqueceu a senha?</a></div>
-            </div>
+                <div id="erro"></div>
             
-        </form>
+                <div class="formgroup">
+                    <label for="email">Email:</label>
+                    <input class="botao" type="email" name="email" id="email" placeholder="skillo@bolotas.com">
+                </div>
+                <div class="formgroup">
+                    <label for="senha">Senha:</label>
+                    <input class="botao" type="password" name="senha" id="senha" placeholder="euAMObolotas123!">
+                </div>
+
+                <button class="botao brilho">Entrar</button>
+            
+                <div class="formgroup">
+                    <input class="lembrar" type="checkbox" name="lembrar">
+                    <label class="lembrar" for="lembrar">Lembre de mim</label>
+                </div>
+            
+                <a href="">Esqueceu a senha?</a>
+            
+            </form>
+        </div>
     </main>
 </body>
 </html>
