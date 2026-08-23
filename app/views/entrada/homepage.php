4c2f227a1ac028527fdc2a6efb3c62eb3333ad8d
Re <134728551+Rebs-Sch@users.noreply.github.com>
2026-08-23 01:17:36 -0300

    Reformulação e restilização da tela inicial
    Rebeca refez a interface e a estrutura da página, para melhorar a sintaxe e a aparência

--- app/views/entrada/homepage.php
+++ app/views/entrada/homepage.php
@@ -1,53 +1,26 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/homepage.css">
+    <link rel="stylesheet" href="assets/css/geralEntrada.css">
+    <link rel="stylesheet" href="assets/css/arruma.css">
+    
     <title>LudoSkill</title>
 </head>
 <body>
     <header>
-        <div id="logo">
-            <div id="logozinha"><img src="assets/imagens/skillo.png" alt=""></div>
-            <a href=""><h2>LudoSkill</h2></a>
-        </div>
-        <div id="botoes">
-            <label for="temas" class="botao">Temas</label>
-            <select name="temas" id="temas" class="botao">
-                <option value="padrao">Padrão</option>
-                <option value="escuro">Escuro</option>
-                <option value="claro">Claro</option>
-            </select>
-
-            <div class="botao">
-                <a href="">Colaboradores</a>
-            </div>
-
-            <div class="botao">
-                <a href="">Sobre nós</a>
-            </div>
-
-            <div class="botao">
-                <a href="">Contato</a>
-            </div>
-
-            <div class="botao verde">
-                <a href="login">Login</a>
-            </div>
-        </div>
+        <?php include_once(__DIR__ . '../includes/menuEntrada.html'); ?>
     </header>
 
     <main>
         <div id="apresentacao">
             <div class="container">
                 <h1>Qualificação de funcionários por <strong>gamificação</strong></h1>
-                <div class="espacinho"></div>
+
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, incidunt explicabo consequatur neque eveniet provident fugiat delectus eum eligendi nemo rem ab dolores necessitatibus omnis magnam adipisci minima voluptatem accusantium eaque numquam, corporis dolorem. Expedita ab quam ipsa natus? Veniam cupiditate sequi possimus incidunt. Ut eum odio minima mollitia reprehenderit.</p>
-                <div class="espacinho"></div>
-                <div class="botao verde">
-                    <a href="">Orçamentos</a>
-                </div>
+
+                <a class="botao brilho" href="">Orçamentos</a>
             </div>
 
             <div class="container">
