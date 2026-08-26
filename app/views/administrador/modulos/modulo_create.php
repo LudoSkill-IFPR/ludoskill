a5274979bea5f691e356866c3d5a06300fd3fa62
Re <134728551+Rebs-Sch@users.noreply.github.com>
2026-08-26 01:19:25 -0300

    Reformulação e restilização da tela de criar módulos
    Rebeca reestruturou o html e ajeitou o css

--- app/views/administrador/modulos/modulo_create.php
+++ app/views/administrador/modulos/modulo_create.php
@@ -3,45 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/geralUsuario.css">
+    <link rel="stylesheet" href="../../assets/css/formulariosAdministrador.css">
+
     <title>Cadastro de Módulos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" >
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
 <!-- implementar aqui uma atenticacao para que apenas administrador possam acessar a pagina de cadastro de modulos -->
-    <div class="container">
-        <div>
 
-            <h1 class="mt-5">Cadastro de Módulos</h1>
-            <form action="<?= URL_BASE ?>/administrador/modulos/salvar" method="post">
-                <div class="mb-3">
+    <header>
+        <?php include_once(__DIR__ . "/../../includes/menuAdministrador.html"); ?>
+    </header>
+
+    <main>
+        <div class="container">
+            
+        <h1 class="">Cadastro de Módulos</h1>
+        <p class="mensagem">Adicione uma nova etapa na trilha de aprendizado.</p>
+
+            <form class="card" action="" method="post">
+                <div class="formgroup">
                     <label for="nome" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" id="nome" name="nome" value="<?= isset($modulo['nome']) ? htmlspecialchars($modulo['nome']) : '' ?>">
+                    <input class="card-secundario" type="text" class="form-control" id="nome" name="nome" value="<?= isset($modulo['nome']) ? htmlspecialchars($modulo['nome']) : '' ?>">
                     <?php if (isset($erros['nome'])): ?>
                         <div class="text-danger"><?= $erros['nome'] ?></div>
                     <?php endif; ?>
                 </div>
-                <div class="mb-3">
+
+                <div class="formgroup">
                     <label for="descricao" class="form-label">Descrição:</label>
-                    <textarea class="form-control" id="descricao" name="descricao"><?= isset($modulo['descricao']) ? htmlspecialchars($modulo['descricao']) : '' ?></textarea>
+                    <textarea class="card-secundario" id="descricao" name="descricao"><?= isset($modulo['descricao']) ? htmlspecialchars($modulo['descricao']) : '' ?></textarea>
                     <?php if (isset($erros['descricao'])): ?>
                         <div class="text-danger"><?= $erros['descricao'] ?></div>
                     <?php endif; ?>
                 </div>
-                <div class="mb-3">
+                
+                <div class="formgroup">
                     <label for="min_estrelas_liberacao" class="form-label">Mínimo de Estrelas para Liberação:</label>
-                    <input type="number" class="form-control" id="min_estrelas_liberacao" name="min_estrelas_liberacao" value="<?= isset($modulo['min_estrelas_liberacao']) ? htmlspecialchars($modulo['min_estrelas_liberacao']) : '' ?>">
+                    <input class="card-secundario" type="number" id="min_estrelas_liberacao" name="min_estrelas_liberacao" value="<?= isset($modulo['min_estrelas_liberacao']) ? htmlspecialchars($modulo['min_estrelas_liberacao']) : '' ?>">
                     <?php if (isset($erros['min_estrelas_liberacao'])): ?>
                         <div class="text-danger"><?= $erros['min_estrelas_liberacao'] ?></div>
                     <?php endif; ?>
                 </div>
-                <button type="submit" class="btn btn-primary">Salvar</button>
-            </form>
+                <button type="submit" class="botao brilho">Salvar</button>
             
-        </div>
-    </div>
-    
+            </form>
+        </div> 
+    </main>
 </body>
 </html>
-
-
