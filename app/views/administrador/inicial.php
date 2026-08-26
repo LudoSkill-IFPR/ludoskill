439f75a99508b6138c9199510878c9d4a0fec24c
Re <134728551+Rebs-Sch@users.noreply.github.com>
2026-08-26 01:18:29 -0300

    corrigindo bosta hihi

--- app/views/administrador/inicial.php
+++ app/views/administrador/inicial.php
@@ -1,54 +1,73 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/css/geralUsuario.css">
-    <link rel="stylesheet" href="../../assets/css/formulariosAdministrador.css">
+    <link rel="stylesheet" href="../../css/geralUsuario.css">
+    <link rel="stylesheet" href="../../css/inicialAdministrador.css">
 
-    <title>Cadastro de Módulos</title>
+    <title>LudoSkill - Administrador</title>
 </head>
 <body>
-<!-- implementar aqui uma atenticacao para que apenas administrador possam acessar a pagina de cadastro de modulos -->
 
     <header>
-        <?php include_once(__DIR__ . "/../../includes/menuAdministrador.html"); ?>
+        <?php include_once(__DIR__ . "/../includes/menuAdministrador.html"); ?>
     </header>
 
     <main>
         <div class="container">
-            
-        <h1 class="">Cadastro de Módulos</h1>
-        <p class="mensagem">Adicione uma nova etapa na trilha de aprendizado.</p>
 
-            <form class="card" action="" method="post">
-                <div class="formgroup">
-                    <label for="nome" class="form-label">Nome:</label>
-                    <input class="card-secundario" type="text" class="form-control" id="nome" name="nome" value="<?= isset($modulo['nome']) ? htmlspecialchars($modulo['nome']) : '' ?>">
-                    <?php if (isset($erros['nome'])): ?>
-                        <div class="text-danger"><?= $erros['nome'] ?></div>
-                    <?php endif; ?>
+            <h1>Área do Administrador</h1>
+            <p class="mensagem">Bem vindo, !</p>
+
+            <section class="infobase">
+                <div class="card">
+                    <h3><i class="bi bi-briefcase-fill"></i> Empresas assinantes</h3>
+                    <p>[numero de empresas no sistema]</p>
                 </div>
 
-                <div class="formgroup">
-                    <label for="descricao" class="form-label">Descrição:</label>
-                    <textarea class="card-secundario" id="descricao" name="descricao"><?= isset($modulo['descricao']) ? htmlspecialchars($modulo['descricao']) : '' ?></textarea>
-                    <?php if (isset($erros['descricao'])): ?>
-                        <div class="text-danger"><?= $erros['descricao'] ?></div>
-                    <?php endif; ?>
+                <div class="card">
+                    <h3><i class="bi bi-person-badge-fill"></i> Gestores cadastrados</h3>
+                    <p>[numero de gestores no sistema]</p>
                 </div>
-                
-                <div class="formgroup">
-                    <label for="min_estrelas_liberacao" class="form-label">Mínimo de Estrelas para Liberação:</label>
-                    <input class="card-secundario" type="number" id="min_estrelas_liberacao" name="min_estrelas_liberacao" value="<?= isset($modulo['min_estrelas_liberacao']) ? htmlspecialchars($modulo['min_estrelas_liberacao']) : '' ?>">
-                    <?php if (isset($erros['min_estrelas_liberacao'])): ?>
-                        <div class="text-danger"><?= $erros['min_estrelas_liberacao'] ?></div>
-                    <?php endif; ?>
+
+                <div class="card">
+                    <h3><i class="bi bi-file-post"></i> Módulos cadastrados</h3>
+                    <p>[quantidade de módulos no sistema]</p>
                 </div>
-                <button type="submit" class="botao brilho">Salvar</button>
-            
-            </form>
-        </div> 
+
+                <div class="card">
+                    <h3><i class="bi bi-tags-fill"></i> Itens cadastrados</h3>
+                    <p>[quantidade de itens no sistema]</p>
+                </div>
+            </section>
+
+            <section class="grafico">
+                <div id="grafico" class="card verde">
+                    <h2><i class="bi bi-graph-up"></i> Gestores por empresa</h2>
+                    [gráfico virá aqui]
+                </div>
+
+                <!-- Aqui vai vir uma espécie de legenda para o gráfico -->
+                <div id="legenda" class="card">
+                    <h3><i class="bi bi-clipboard-data-fill"></i> Empresas mais presentes</h3>
+
+                    <ol>
+                        <li class="card-secundario">
+                        </li>
+                        <li class="card-secundario">
+                        </li>
+                        <li class="card-secundario">
+                        </li>
+                        <li class="card-secundario">
+                        </li>
+                        <li class="card-secundario">
+                        </li>
+                    </ol>
+                </div>
+            </section>
+        </div>
     </main>
+
 </body>
 </html>
