1e78097111b9d277eb69a000c4c02da11cc36674
Re <134728551+Rebs-Sch@users.noreply.github.com>
2026-08-26 23:18:37 -0300

    correção de título 2

--- app/views/administrador/modulos/modulo_edit.php
+++ app/views/administrador/modulos/modulo_edit.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="../../assets/css/geralUsuario.css">
     <link rel="stylesheet" href="../../assets/css/formulariosAdministrador.css">
     
-    <title>LudoSkill - Editar de Módulo</title>
+    <title>LudoSkill - Editar Módulo</title>
 </head>
 <body>
 
