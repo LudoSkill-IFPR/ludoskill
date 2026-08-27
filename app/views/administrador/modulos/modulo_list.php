2b0c9e2c04f535028aaeb297a6c6eaabeb2883f5
Re <134728551+Rebs-Sch@users.noreply.github.com>
2026-08-26 22:28:11 -0300

    Esqueci de mudar o caminho do css. Corrigi

--- app/views/administrador/modulos/modulo_list.php
+++ app/views/administrador/modulos/modulo_list.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../css/geralUsuario.css">
-    <link rel="stylesheet" href="../../../css/formulariosAdministrador.css">
+    <link rel="stylesheet" href="../../assets/css/geralUsuario.css">
+    <link rel="stylesheet" href="../../assets/css/formulariosAdministrador.css">
     
     <title>LudoSkill - Listagem de Módulos</title>
 </head>
