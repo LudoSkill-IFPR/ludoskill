d5f7b7a7b8e0dcb8a1e93e03addc0104d7721096
Re <134728551+Rebs-Sch@users.noreply.github.com>
2026-08-27 00:58:44 -0300

    Reformulação e restilização da tela de listar funcionários

--- app/views/administrador/funcionario/funcionario_list.php
+++ app/views/administrador/funcionario/funcionario_list.php
@@ -9,63 +9,78 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de Funcionarios</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" >
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../../assets/css/geralUsuario.css">
+    <link rel="stylesheet" href="../../assets/css/formulariosAdministrador.css">
+    
+    <title>LudoSkill - Listar Funcionários</title>
 </head>
+
 <body>
-    <div class="container mt-3">
-        <a href="<?= URL_BASE ?>/gestor/funcionarios/cadastrar" class="btn btn-primary mb-3">
-            <i class="bi bi-plus"></i> Novo Funcionario
-        </a>
-    </div>
+    <header>
+        <?php include_once(__DIR__ . "/../../includes/menuAdministrador.html"); ?>
+    </header>
 
-    <div class="container">
-        <h2>Listagem de Funcionarios</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Data de Nascimento</th>
-                    <th>CPF</th>
-                    <th>E-mail</th>
-                    <th>Numero de Telefone</th>
-                    <th>Empresa</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($funcionarios as $funcionario): ?>
-                    <?php
-                        $nome_empresa = $empresaRepository->getEmpresaById($funcionario['id_empresa']);
-                        $nome_empresa = $nome_empresa['nome'];
-                        $usuario = $usuarioRepository->getUsuario($funcionario['id_usuario']);
-                    ?>
-                    <tr>
-                        <td><?= htmlspecialchars($usuario['nome_completo']) ?></td>
-                        <td><?= htmlspecialchars($usuario['data_nascimento']) ?></td>
-                        <td><?= htmlspecialchars($usuario['CPF']) ?></td>
-                        <td><?= htmlspecialchars($usuario['email']) ?></td>
-                        <td><?= htmlspecialchars($usuario['numero_telefone']) ?></td>
+    <main>
+        <div class="container">
+            <div id="topo">
+                <a href="/administrador/inicial" class="botao brilho"><i class="bi bi-arrow-left"></i> Voltar</a>
+                <a href="<?= URL_BASE ?>/gestor/funcionarios/cadastrar" class="botao brilho"><i class="bi bi-plus"></i> Novo Funcionário</a>
 
-                        <td class="px-4 py-3 align-middle text-end">
-                            <a href="<?= URL_BASE ?>/gestor/funcionarios/editar?id=<?= $funcionario['id_funcionario'] ?>" class="btn btn-sm btn-outline-primary">
-                                <i class="bi bi-pencil"></i> Editar
-                            </a>
-                            <form action="<?= URL_BASE ?>/gestor/funcionarios/excluir" method="post" class="d-inline" onsubmit="return confirm('Deseja excluir o(a) funcionario(a) <?= $usuario['nome_completo']?>?')">
-                                <input type="hidden" name="id" value="<?= htmlspecialchars($funcionario['id_funcionario'] ?? '') ?>">
-                                <button type="submit" class="btn btn-sm btn-outline-danger">
-                                    <i class="bi bi-trash"></i> Excluir
-                                </button>
-                            </form>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
+                <div>
+                    <h1>Listagem de Funcionários</h1>
+                    <p class="mensagem">Visualize os aprendizes cadastrados.</p>
+                </div>
+            </div>
+    
+            <section class="card">
+                <h2>Lista de Itens</h2>
+                <table id="funcionario">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Data de Nascimento</th>
+                            <th>CPF</th>
+                            <th>E-mail</th>
+                            <th>Numero de Telefone</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php foreach ($funcionarios as $funcionario): ?>
+                            <?php
+                            $nome_empresa = $empresaRepository->getEmpresaById($funcionario['id_empresa']);
+                            $nome_empresa = $nome_empresa['nome'];
+                            $usuario = $usuarioRepository->getUsuario($funcionario['id_usuario']);
+                        ?>
+                        <tr>
+                            <td><?= htmlspecialchars($usuario['nome_completo']) ?></td>
+                            <td><?= htmlspecialchars($usuario['data_nascimento']) ?></td>
+                            <td><?= htmlspecialchars($usuario['CPF']) ?></td>
+                            <td><?= htmlspecialchars($usuario['email']) ?></td>
+                            <td><?= htmlspecialchars($usuario['numero_telefone']) ?></td>
+                    
+                            <td id="acao">
+                                <a href="<?= URL_BASE ?>/gestor/funcionarios/editar?id=<?= $funcionario['id_funcionario'] ?>" class="botao brilho"><i class="bi bi-pencil"></i> Editar</a>
+                                
+                                <form action="<?= URL_BASE ?>/gestor/funcionarios/excluir" method="post" onsubmit="return confirm('Deseja excluir o(a) funcionario(a) <?= $usuario['nome_completo']?>?')">
+                                    <input type="hidden" name="id" value="<?= htmlspecialchars($funcionario['id_funcionario'] ?? '') ?>">
+                                    <button type="submit" class="botao brilhinho"><i class="bi bi-trash"></i> Excluir</button>
+                                </form>
+
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+
+                </table>
+            </section>
+
+        </div>
+    </main>
 </body>
 </html>
